9d77fbc8f95805ece74895361f8e22d64ed9bbc7
azu <azuciao@gmail.com>
2018-03-17 20:34:29 +0900

    use analytics

--- src/ga/provide.js
+++ src/ga/provide.js
@@ -1,13 +1,10 @@
-// MIT © 2018 azu
-"use strict";
-
 /**
  * Provides a plugin name and constructor function to analytics.js.
  * @param {string} pluginName
  * @param {Function} pluginConstructor
  */
 export function providePlugin(pluginName, pluginConstructor) {
-    const ga = window['GoogleAnalyticsObject'] || window['ga'] || window['gtag'];
+    const ga = window[window['GoogleAnalyticsObject'] || 'ga'];
     if (typeof ga === 'function') {
         ga('provide', pluginName, pluginConstructor);
     }
